c086d608f9a4e6de37700861addea5c7422d8e3a
David Senen Ruiz <david.senenruiz@sainsburys.co.uk>
2024-09-06 16:00:24 +0100

    Making it work

--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -4,6 +4,7 @@ export const StyledFooter = styled.footer`
     background-color: ${({ theme }) => theme.colors.footer};
     color: #fff;
     padding: 100px 0 60px;
+    text-align: center;
 
     ul {
         list-style-type: none;
